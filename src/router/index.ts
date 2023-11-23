b8d723495fb62f6d039396ad724cb247fd37ae71
nemkicsiaron <aronfszabo@gmail.com>
2023-11-23 16:16:56 +0100

    personalize a bit

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,15 +7,20 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/success',
+      name: 'success',
+      component: () => {}
     }
   ]
 })
