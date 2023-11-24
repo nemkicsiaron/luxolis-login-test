1270381a7929b50c3d20bd868f3837f74b308064
nemkicsiaron <aronfszabo@gmail.com>
2023-11-24 16:35:34 +0100

    rudimentary nav guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
@@ -11,18 +12,17 @@ const router = createRouter({
 		{
 			path: '/login',
 			name: 'login',
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
 			component: () => import('../views/LoginView.vue')
 		},
 		{
 			path: '/success',
 			name: 'success',
-			meta: {
-				requiresAuth: true
+			beforeEnter: (to, from, next) => {
+				console.log(to, from);
+				if(from.name !== 'login') next('/login');
+				else next();
 			},
-			component: () => {}
+			component: () => import('../views/SuccessView.vue')
 		}
 	]
 });
---
+++ src/views/SuccessView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+console.log("Congrats! You logged in");
+</script>
+
+<template>
+	<div>
+	</div>
+</template>
